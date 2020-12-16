8abb7e5d59f971d032852faaa6faa82f8bf95d24
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-16 19:43:45 +0200

    .env added

---
+++ server/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "translator",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+  test: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_TEST || "translator_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+  production: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "translator",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const app = require("./app");
-// require("dotenv").config();
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+require("dotenv").config();
 
+const app = require("./app");
 const httpSocket = require("./socket");
+
+const PORT = process.env.PORT || 8080;
+const PORT_SOCKET = process.env.PORT_SOCKET || 8081;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-httpSocket.listen(8081, () => console.log("socket is listening..."));
+httpSocket.listen(PORT_SOCKET, () => {
+  console.log(`Socket is listening on port ${PORT_SOCKET}`);
+});
