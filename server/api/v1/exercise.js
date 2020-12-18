a4f2bd54ea9d7ae36b12b8beb208dcfd81ed9de9
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-18 18:11:33 +0200

    fixed text2speech server

--- server/api/v1/exercise.js
+++ server/api/v1/exercise.js
@@ -3,34 +3,28 @@ const { User, Language } = require("../../models");
 const router = Router();
 const { text2speech } = require("../../google-api/text2speech");
 
-let mySetting = {
-  audioConfig: {
-    audioEncoding: "LINEAR16",
-    pitch: 0,
-    speakingRate: 0.7,
-  },
-  input: {
-    text: "hello, my name is Dor",
-  },
-  voice: {
-    languageCode: "en-US",
-    name: "en-US-Wavenet-F",
-  },
-
-  // outputFileNmae: "output1.mp3",
-};
-
 router.post("/", async (req, res) => {
   try {
     const userInfo = await User.findOne({
       where: { id: 1 },
       include: [
-        { model: Language, attributes: ["code", "language"], as: "nativeLanguage" },
-        { model: Language, attributes: ["code", "language"], as: "currentLanguage" },
+        { model: Language, as: "nativeLanguage" },
+        { model: Language, as: "currentLanguage" },
       ],
     });
-    // res.json(userInfo);
-    obj = await text2speech(mySetting);
+    if (req.body.textInput) {
+      obj = await text2speech({
+        inputText: req.body.textInput,
+        languageCode: userInfo.nativeLanguage.code,
+        voiceName: userInfo.nativeLanguage.voice,
+      });
+    } else {
+      obj = await text2speech({
+        inputText: "hello, my name is dodo",
+        languageCode: userInfo.currentLanguage.code,
+        voiceName: userInfo.currentLanguage.voice,
+      });
+    }
     res.json(obj);
   } catch (error) {
     console.error(error);
--- server/google-api/text2speech.js
+++ server/google-api/text2speech.js
@@ -1,20 +1,26 @@
 const textToSpeech = require("@google-cloud/text-to-speech");
-const fs = require("fs");
-const util = require("util");
 const projectID = "first";
 const keyFilename = "./google-api/google-api-keys.json";
 const client = new textToSpeech.TextToSpeechClient({ projectID, keyFilename });
 
-async function text2speech(MySettings) {
-  const [response] = await client.synthesizeSpeech(MySettings);
+async function text2speech({ inputText, languageCode, voiceName }) {
+  let mySetting = {
+    audioConfig: {
+      audioEncoding: "LINEAR16",
+      pitch: 0,
+      speakingRate: 0.9,
+    },
+    input: {
+      text: inputText,
+    },
+    voice: {
+      languageCode: languageCode,
+      name: voiceName,
+    },
+  };
+  const [response] = await client.synthesizeSpeech(mySetting);
 
   return response.audioContent.toString("base64");
-  // fs.writeFileSync("bytes.json", JSON.stringify(response.audioContent.toString("base64")));
-  // fs.writeFileSync("bytes.json", JSON.stringify({ name: "dor" }));
-
-  //   const writeFile = util.promisify(fs.writeFile);
-  //   await writeFile("output.mp3", response.audioContent, "binary");
-  //   console.log("Audio content written to file: output.mp3");
 }
 
 module.exports.text2speech = text2speech;
