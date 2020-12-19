fe1251f71d10900265a7f4db28c0a4a9c063d281
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-19 19:10:29 +0200

    text2speech works with the server

--- client/src/components/textToSpeech/Text2speech.js
+++ client/src/components/textToSpeech/Text2speech.js
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ReactAudioPlayer from "react-audio-player";
+// import ReactAudioPlayer from "react-audio-player";
 import LoopIcon from "@material-ui/icons/Loop";
 import "./style.css";
+
 export default function Text2speech() {
   const [audio, setAudio] = useState();
   const [nameOfClass, setNameOfClass] = useState("play-again");
+  const [counter, setCounter] = useState(0);
+  const [stop, setStop] = useState(false);
 
   console.log(audio);
-  useEffect(async () => {
-    const { data } = await axios.post("/api/v1/exercise", { textInput: "hey there" });
-    setAudio(data);
+  useEffect(() => {
+    (async () => {
+      // const { data } = await axios.post("/api/v1/exercise", {
+      //   textInput: `hey ${"dor"}, how are you?`,
+      // });
+      const { data } = await axios.post("/api/v1/exercise");
+      console.log(data);
+      setAudio(data);
+    })();
   }, []);
 
-  useEffect(async () => {
-    const audioToPlay = new Audio(`data:audio/ogg;base64, ${audio}`);
-    audioToPlay.play();
-  }, [audio]);
+  useEffect(() => {
+    (async () => {
+      if (audio && !stop) {
+        playAudio();
+      }
+    })();
+  }, [audio, counter]);
+
+  const playAudio = () => {
+    debugger;
+    if (counter < audio.length) {
+      const audioToPlay = new Audio(`data:audio/ogg;base64, ${audio[counter].base64}`);
+      audioToPlay.play();
+      audioToPlay.onended = () => {
+        setCounter((prev) => prev + 1);
+      };
+    } else {
+      setStop(true);
+      setCounter(0);
+    }
+  };
 
   const playAgain = () => {
-    const audioToPlay = new Audio(`data:audio/ogg;base64, ${audio}`);
-    audioToPlay.play();
+    playAudio();
   };
   const MouseDown = () => {
     setNameOfClass("play-again down");
@@ -38,8 +63,13 @@ export default function Text2speech() {
       >
         <LoopIcon style={{ fontSize: "40px", color: "white" }} />
       </button>
-      {/* <div>{audio && <audio controls autoPlay src={`data:audio/ogg;base64, ${audio}`}></audio>}</div> */}
-      {/* {audio && <ReactAudioPlayer src={`data:audio/ogg;base64, ${audio}`} autoPlay controls />} */}
+      {audio && (
+        <div>
+          {audio.map((obj) => (
+            <span className={obj.itsWord ? "word-bold" : "regular"}>{obj.text} </span>
+          ))}
+        </div>
+      )}
     </>
   );
 }
