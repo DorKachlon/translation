8537da13bcc9e9a9ee6ef0c6271f1ccfacfffa4d
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-19 20:40:00 +0200

    dialog router created

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -4,6 +4,7 @@ const router = Router();
 router.use("/translation", require("./translation"));
 router.use("/exercise", require("./exercise"));
 router.use("/feedback", require("./feedback"));
+router.use("/dialog", require("./dialog"));
 
 router.use("/languages", require("./languages"));
 router.use("/users", require("./users"));
