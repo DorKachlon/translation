2872ad99ad268e7788c4567918eacf7aa09e6d8e
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-25 17:27:24 +0200

    update app js works with loggedin context

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,79 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Setting from "./pages/Setting";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Landing from "./pages/Landing";
-
+import { Logged } from "./context/LoggedIn";
+import Cookies from "js-cookie";
+import network from "./services/network";
+import "./App.css";
+import Loading from "./pages/Loading";
 import NavBar from "./components/navBar";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { grey } from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const myTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+  },
+});
 
 function App() {
+  const [logged, setLogged] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // auth
+    (async () => {
+      try {
+        if (Cookies.get("accessToken")) {
+          const { data } = await network.get("/api/v1/auth/validate-token");
+          console.log(data);
+          setLogged(data.valid);
+          setLoading(false);
+        } else if (Cookies.get("refreshToken")) {
+          await network.post("/api/v1/auth/token", { token: Cookies.get("refreshToken") });
+          setLoading(false);
+        } else {
+          setLoading(false);
+        }
+      } catch (error) {
+        setLoading(false);
+      }
+    })();
+  }, []);
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/setting" component={Setting} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/landing" component={Landing} />
-        </Switch>
-      </Router>
+      <ThemeProvider theme={myTheme}>
+        <Logged.Provider value={{ logged, setLogged }}>
+          <Router>
+            <NavBar />
+            {!loading ? (
+              logged ? (
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/setting" component={Setting} />
+                  {/* <Route exact path="/login" component={Login} /> */}
+                  {/* <Route exact path="/sign-up" component={SignUp} /> */}
+                </Switch>
+              ) : (
+                <Switch>
+                  <Route exact path="/" component={Landing} />
+                  {/* <Route exact path="/setting" component={Setting} /> */}
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/sign-up" component={SignUp} />
+                </Switch>
+              )
+            ) : (
+              <Loading />
+            )}
+          </Router>
+        </Logged.Provider>
+      </ThemeProvider>
     </div>
   );
 }
