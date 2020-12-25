40dcc82c8834e7eb9323be475ad63f80b9925245
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-25 20:21:02 +0200

    ranme recorder

--- client/src/components/recorder/index.js
+++ client/src/components/recorder/index.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
-// import Recorder from "../../helper/Recorder";
 import useSound from "use-sound";
 import SoundIn from "../../sound-effect/sound-in.mp3";
 import SoundOut from "../../sound-effect/sound-out.mp3";
 import Analyzer from "../../ts/AudioAnalyzer";
 import MicIcon from "@material-ui/icons/Mic";
-import "./style.css";
 import Text2speech from "../textToSpeech/Text2speech";
 import useRecorder from "../../hook/useRecorder";
+import "./style.css";
 
-export default function Records7() {
+export default function Recorder() {
   const [saidWord, setSaidWord] = useState("");
 
   const [playSoundIn] = useSound(SoundIn);
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Records7 from "../../components/recorder/Records7";
-import Text2speech from "../../components/textToSpeech/Text2speech";
+import Recorder from "../../components/recorder";
 
 export default function Home() {
   return (
     <div>
-      <Records7 />
-      {/* <Text2speech /> */}
+      <Recorder />
     </div>
   );
 }
