8bbea11c52eb5e9c859e358ea08fc3eb604ab8f5
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-25 17:28:02 +0200

    navbar new style and works with context

--- client/src/components/navBar/index.js
+++ client/src/components/navBar/index.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import Logout from "../Logout";
-
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { MyTab } from "./styled";
+import { NavLink, Link } from "react-router-dom";
+import logo from "./logo.png";
+import "./style.css";
+import { Logged } from "../../context/LoggedIn";
 export default function NavBar() {
+  const [value, setValue] = useState(0);
+  const LoggedContext = useContext(Logged);
+  console.log(LoggedContext.logged);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div style={{ position: "absolute" }}>
-      <button>
-        <a href="/">home</a>
-      </button>
-      <button>
-        <a href="/setting">setting</a>
-      </button>
-      <button>
-        <a href="/login">login</a>
-      </button>
-      <button>
-        <a href="/sign-up">sign up</a>
-      </button>
-      <Logout />
-    </div>
+    <>
+      {LoggedContext.logged ? (
+        <>
+          <NavLink to="/" exact>
+            <img className="navbar-logo" src={logo} alt="logo" border="0" />
+          </NavLink>
+          <div style={{ position: "absolute", right: "0", zIndex: "999" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="Home" component={Link} to="/" />
+              <Tab label="Setting" component={Link} to="/setting" />
+            </Tabs>
+            <Logout />
+          </div>
+        </>
+      ) : (
+        <>
+          <NavLink to="/" exact>
+            <img className="navbar-logo" src={logo} alt="logo" border="0" />
+          </NavLink>
+          <div style={{ position: "absolute", right: "0", zIndex: "999" }}>
+            <MyTab label="Login" component={Link} to="/login" />
+            <MyTab label="Sign Up" component={Link} to="/sign-up" />
+          </div>
+        </>
+      )}
+    </>
   );
 }
---
+++ client/src/components/navBar/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Tab from "@material-ui/core/Tab";
+
+export const MyTab = styled(Tab)`
+  background-color: #ffa230 !important;
+  opacity: 1 !important;
+  border-radius: 50px !important;
+  margin: 15px !important;
+`;
