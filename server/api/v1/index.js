6dc14d98871922f565228a434ee07a3a76ec17b2
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-25 20:18:03 +0200

    middleware for develop - stop req

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 const router = Router();
 const verifyToken = require("../../middleware/verifyToken");
 const progress = require("../../middleware/progress");
+const stop = require("../../middleware/onlyForDevelop");
 
 // router.use("/translation", require("./translation"));
 
-router.use("/exercise", verifyToken, progress, require("./exercise"));
-router.use("/answer", verifyToken, progress, require("./answer"));
+router.use("/exercise", stop, verifyToken, progress, require("./exercise"));
+router.use("/answer", stop, verifyToken, progress, require("./answer"));
 // router.use("/feedback", require("./feedback"));
 
 // router.use("/dialog", require("./dialog"));
