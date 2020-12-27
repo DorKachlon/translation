1b812c270260ff7b9816748bea3e5882ea80eb4d
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-27 18:29:05 +0200

    loading for setting page

--- client/src/pages/Setting/index.js
+++ client/src/pages/Setting/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import SettingSelectors from "../../components/selectors/SettingSelectors";
+import Loading from "../Loading";
 import "./style.css";
 export default function Setting() {
+  const [loading, setLoading] = useState(true);
   return (
-    <div className="setting-page">
-      <div className="setting-selectors">
-        <SettingSelectors />
+    <>
+      {loading && <Loading />}
+      <div className="setting-page">
+        <div className="setting-selectors">
+          <SettingSelectors setLoading={setLoading} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
