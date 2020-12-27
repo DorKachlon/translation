7e2f53460d819cf7e3e1df3fbed320e391c6171c
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-27 14:03:20 +0200

    createSpeech works with speechCache

--- server/helperFunctions/createSpeech.js
+++ server/helperFunctions/createSpeech.js
@@ -1,5 +1,7 @@
 const { text2speech } = require("../google-api/text2speech");
 const { translate } = require("./translation");
+const { SpeechCache } = require("../models");
+const fs = require("fs").promises;
 
 //text=`you said : <apple> and you need to said <water>, try again`
 async function createSpeech(text, l1, l2) {
@@ -25,11 +27,7 @@ async function createSpeech(text, l1, l2) {
 
         arrOfAudio.push({
           text: textAfterTranslation,
-          base64: await text2speech({
-            inputText: textAfterTranslation,
-            languageCode: textAndLanguage.language.code,
-            voiceName: textAndLanguage.language.voice,
-          }),
+          base64: await speechHelper(textAfterTranslation, textAndLanguage.language),
           itsWord: textAndLanguage.itsWord,
         });
       }
@@ -39,4 +37,35 @@ async function createSpeech(text, l1, l2) {
     console.error(error);
   }
 }
+
+async function speechHelper(text, language) {
+  try {
+    let speech;
+    const speechDb = await SpeechCache.findOne({ where: { text, languageId: language.id } });
+    if (speechDb) {
+      speech = await fs.readFile(`${__dirname}/../speechCache/${speechDb.fileName}.json`);
+      return JSON.parse(speech).base64;
+    }
+    console.log(`request speech from google api for: ${text}`);
+    speech = await text2speech({
+      inputText: text,
+      languageCode: language.code,
+      voiceName: language.voice,
+    });
+    const obj = {
+      text,
+      languageId: language.id,
+      fileName: new Date().getTime().toString(),
+    };
+    await fs.writeFile(
+      `${__dirname}/../speechCache/${obj.fileName}.json`,
+      JSON.stringify({ base64: speech })
+    );
+    await SpeechCache.create(obj);
+    return speech;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 module.exports.createSpeech = createSpeech;
