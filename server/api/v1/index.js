cd104b0cded41261cddddb8de53cbc6366f40c89
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-28 19:09:10 +0200

    progress router server side

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,21 +1,22 @@
 const { Router } = require("express");
 const router = Router();
 const verifyToken = require("../../middleware/verifyToken");
-const progress = require("../../middleware/progress");
+const progressMiddleware = require("../../middleware/progress");
 const stop = require("../../middleware/onlyForDevelop");
 
 // router.use("/exercise", stop, verifyToken, progress, require("./exercise"));
 // router.use("/answer", stop, verifyToken, progress, require("./answer"));
 
-router.use("/exercise", verifyToken, progress, require("./exercise"));
-router.use("/answer", verifyToken, progress, require("./answer"));
+router.use("/exercise", verifyToken, progressMiddleware, require("./exercise"));
+router.use("/answer", verifyToken, progressMiddleware, require("./answer"));
 
 // router.use("/translation", require("./translation"));
 // router.use("/feedback", require("./feedback"));
 // router.use("/dialog", require("./dialog"));
 
 router.use("/languages", require("./languages"));
-router.use("/users", verifyToken, progress, require("./users"));
+router.use("/users", verifyToken, progressMiddleware, require("./users"));
 router.use("/auth", require("./authentication"));
+router.use("/progress", verifyToken, require("./progress"));
 
 module.exports = router;
---
+++ server/api/v1/progress.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const router = Router();
+const { Progress, Word, Language } = require("../../models");
+const sequelize = require("sequelize");
+
+router.get("/", async (req, res) => {
+  const languagesInProgress = await Progress.findAll({
+    where: { userId: req.user.id },
+    group: "language_id",
+    attributes: ["languageId"],
+    include: [{ model: Language, as: "language" }],
+  });
+  const wordsForUserByLanguages = await Promise.all(
+    languagesInProgress.map(async (language) => {
+      const progressByUser = await Progress.findAll({
+        attributes: [[sequelize.fn("sum", sequelize.col("score")), "totalScore"]],
+        where: { userId: req.user.id, languageId: language.languageId },
+        group: "word_id",
+        include: [{ model: Word }],
+      });
+      console.log(progressByUser);
+      return { language: language.language, words: progressByUser };
+    })
+  );
+  res.json(wordsForUserByLanguages);
+});
+module.exports = router;
