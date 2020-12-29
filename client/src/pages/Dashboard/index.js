e1251e08aade5b5ac77c37a8c5e8a632754d618a
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-29 18:14:49 +0200

    create gauge and use in dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import network from "../../services/network";
 import "./style.css";
 import Loading from "../Loading";
+import Gauge from "../../components/gauge";
 export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [wordsByLanguages, setWordsByLanguages] = useState();
@@ -27,7 +28,7 @@ export default function Dashboard() {
                   {wordsForLangue.words.map((wordAndTotalScore) => (
                     <div className="dashboard-word-container">
                       <div>{wordAndTotalScore.Word.word}</div>
-                      <div>{wordAndTotalScore.totalScore}</div>
+                      <Gauge value={wordAndTotalScore.totalScore} />
                     </div>
                   ))}
                 </div>
