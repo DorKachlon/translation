e7d7095c939713c49b86ae8e1c2e54aa9516a907
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-12-31 00:10:43 +0200

    new style landing page with animations

---
+++ client/src/components/volume/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+export default function Volume() {
+  return (
+    <div className="volume-container">
+      <div className="line line-1"></div>
+      <div className="line line-2"></div>
+      <div className="line line-3"></div>
+      <div className="line line-4"></div>
+      <div className="line line-5"></div>
+      <div className="line line-6"></div>
+      <div className="line line-7"></div>
+      <div className="line line-8"></div>
+    </div>
+  );
+}
--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -3,7 +3,7 @@ import LeftBottom from "./leftBottom.svg";
 import Girl from "./girl.svg";
 import RightTop from "./rightTop.svg";
 import rightBottom from "./rightBottom.svg";
-
+import Volume from "../../components/volume";
 import "./style.css";
 
 export default function Landing() {
@@ -13,8 +13,11 @@ export default function Landing() {
       <img className="girl" src={Girl} alt="" />
       <img className="right-top" src={RightTop} alt="" />
       <img className="right-bottom" src={rightBottom} alt="" />
+      <div className="landing-volume">
+        <Volume />
+      </div>
       <div className="landing-paragraph">
-        <div className="landing-title-1">Better then ever</div>
+        <div className="landing-title-1">Better than ever</div>
         <div className="landing-title-2">Anna lets you learn</div>
         <div className="landing-title-3">without lifting a finger.</div>
       </div>
